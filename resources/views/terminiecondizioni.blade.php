31257215c7ef60b50f6884107009e6c0d322c7b8
enricopierga <S1093538@studenti.univpm.it>
2022-05-21 14:26:52 +0200

    rotte rermini e condizioni

--- resources/views/terminiecondizioni.blade.php
+++ resources/views/terminiecondizioni.blade.php
@@ -33,7 +33,7 @@
     <!-- Navigation bar-->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container">
-            <a class="navbar-brand logo-image" href="index.html"><img src="images/logo.png"></a> 
+            <a class="navbar-brand logo-image" href="{{route('home')}}"><img src="images/logo.png"></a> 
 
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav ml-auto">
@@ -41,7 +41,7 @@
                         <a class="nav-link page-scroll" href="#description">DESCRIZIONE</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll" href="accedi.html">ACCEDI</a>
+                        <a class="nav-link page-scroll" href="{{route('accedi')}}">ACCEDI</a>
                     </li>
                 </ul>
                 <span class="nav-item">
